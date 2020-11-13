9988f2b05f4a95eab40f0e47663cafe5f2f3a134
BorisBBoychev <borisboychev007@Gmail.com>
2020-11-13 17:31:26 +0200

    user gender option

--- ecom/api/user/models.py
+++ ecom/api/user/models.py
@@ -4,6 +4,18 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
+    MALE = 'male'
+    FEMALE = 'female'
+    OTHER = 'other'
+    UNKNOWN = 'unknown'
+
+    GENDER_TYPES = (
+        (MALE, 'male'),
+        (FEMALE,'female'),
+        (OTHER, 'other'),
+        (UNKNOWN, 'unknown'),
+    )
+
     name = models.CharField(max_length=60, default='ANONYMOUS')
     email = models.EmailField(max_length=250, unique=True)
 
@@ -13,10 +25,10 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     phone = models.CharField(max_length=20, blank=True, null=True)
-    gender = models.CharField(max_length=10, blank=True, null=True)
+    gender = models.CharField(max_length=8, choices=GENDER_TYPES, default=UNKNOWN)
     address = models.CharField(max_length=250, blank=True, null=True)
 
-    session_token = models.CharField(max_length=10, default=0)
+    session_token = models.CharField(max_length=32, default=0)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
